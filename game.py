7e1a84af9aade383cf487b315dce4e98a7625d94
YM2992 <yasirm427@gmail.com>
2022-05-31 21:03:12 +1000

    Added clicking and dragging functions. Bot now automatically adds ingredients in a circle.

--- game.py
+++ game.py
@@ -1,50 +1,86 @@
-from turtle import width
-
+import math
+import pyautogui
+import pygetwindow as gw
+import time
+import string
 
 class Game:
-    def __init__(self, capture_width, capture_height):
-        # Customer dialogue text box position
-        self.update_capture_bounds(capture_width, capture_height)
+    # Current states
+    _making_pizza = False
 
-        # Ingredients
-        self.valid_ingredients = ["sauce", "cheese", "pepperoni", "sausage", "mushroom", "olive"]
+    # Ingredients
+    valid_ingredients = ["sauce", "cheese", "pepperoni", "sausage", "mushroom", "olive", "eggplant"]
+    requested_ingredients = []
+    # Positions
+    ingredient_positions = {
+        "base_0": (795, 680),
+        "dough_0": (215, 485),
+        "dough_1": (375, 485),
+        "dough_2": (215, 600),
+        "dough_3": (375, 600),
+        "dough_4": (215, 715),
+        "dough_5": (375, 715),
+        "dough_6": (215, 830),
+        "dough_7": (375, 830),
+        "sauce": (275, 235),
+        "cheese": (645, 235),
+        "pepperoni": (935, 235),
+        "sausage": (1235, 235),
+        "mushroom": (1530, 235),
+        "eggplant": (1820, 235),
+        "olive": (1820, 510),
+    }
+    # Counts
+    dough_count = 0
 
-        # Recipes
-        self.recipes = {
-            "cheese": ["sauce"]
-        }
+    def __init__(self, window_name):
+        # Get the window of the phone ADB/scrcpy application to capture
+        self.window = gw.getWindowsWithTitle(window_name)
+        while len(self.window) == 0:
+            self.window = gw.getWindowsWithTitle(window_name)
+            time.sleep(0.1)
+        self.window = self.window[0]
+        self.window.activate()
 
-    def update_capture_bounds(self, new_width, new_height):
-        self.capture_width, self.capture_height = new_width, new_height
-        
+        print(f"w: {self.window.width}, h: {self.window.height}")
+
+        # Update customer dialogue text box bounds
+        self.update_text_bounds()
+    
+    # Update the bounds of text capture when the ABD window is resized
+    def update_text_bounds(self):        
         self.text_bounds = {
-            'x': int(570 / 2352 * self.capture_width),
-            'y': int(170 / 1119 * self.capture_height),
-            'width': int(1500 / 2352 * self.capture_width),
-            'height': int(315 / 1119 * self.capture_height)
+            'x': int(570 / 2352 * self.window.width),
+            'y': int(175 / 1119 * self.window.height),
+            'width': int(1500 / 2352 * self.window.width),
+            'height': int(330 / 1119 * self.window.height)
         }
 
         self.text_bounds["width"] = self.text_bounds["x"] + self.text_bounds["width"]
         self.text_bounds["height"] = self.text_bounds["y"] + self.text_bounds["height"]
-        
+    
+    # Capture and return a screenshot of the ADB window
+    def get_screenshot(self):
+        return pyautogui.screenshot(region=(self.window.left, self.window.top, self.window.width, self.window.height))
+    
+    # Get ingredients from a sentence
+    def get_ingredients(self, sentence):
+        if self._making_pizza:
+            return "Currently making pizza"
 
-    def get_recipe(self, words):
         self.requested_ingredients = []
-        words = str(words).lower().strip(".")
-        for index, word in enumerate(words.split()):
+        sentence = str(sentence).lower().translate(str.maketrans('', '', string.punctuation))
+        for index, word in enumerate(sentence.split()):
             word = str(word).lower()
-            print(word)
 
             if word not in self.valid_ingredients:
                 # If part of the word exists in valid_ingredients. Example: "olives" or "olives!"
                 if word[:len(word)-1] not in self.valid_ingredients:
                     word = word[:len(word)-1] # Cut word down one letter. Example: "olive" or "olives"
-                print(word)
                 # If part of the word exists in valid_ingredients. Example: "olives" or "olives!"
                 if word not in self.valid_ingredients:
                     word = word[:len(word)-1] # Cut word down one letter. Example: "olive" or "olives"
                 
-                print(word)
                 if word not in self.valid_ingredients:
                     continue
 
@@ -58,14 +94,114 @@ class Game:
                 continue
 
             # Handle requests that ask for none of that ingredient
-            if words[index - 1] == "no":
+            if sentence[index - 1] == "no":
                 continue
-
             if "less" in word:
                 continue
 
             # Ingredient is acceptable, add it to the list
             self.requested_ingredients.append(word)
             continue
+            
+        # Add sauce and cheese if they weren't added but should be
+        if "sauce" not in self.requested_ingredients and "sauce" in sentence:
+            self.requested_ingredients.insert(0, "sauce")
+
+        if "cheese" not in self.requested_ingredients and "cheese" in sentence:
+            self.requested_ingredients.insert(0, "cheese")
         
         return self.requested_ingredients
+
+    def get_recipe(self):
+        pass
+
+    def get_ingredient_position(self, ingredient):
+        return self.ingredient_positions[ingredient]
+
+    def place_in_circle(self):
+        total_ingredients = 18
+        theta = math.radians(360 / total_ingredients)
+        radius = 160
+
+        centre_x, centre_y = self.get_ingredient_position("base_0")
+
+        for i in range(1, total_ingredients + 1):
+            theta = math.radians(360 / total_ingredients * i)
+            x = radius * math.cos(theta) + centre_x
+            y = radius * math.sin(theta) + centre_y
+            self.click(x, y)
+
+    def make_pizza(self):
+        if self._making_pizza:
+            return "Already making pizza"
+        elif len(self.requested_ingredients) == 0:
+            return "0 ingredients requested on pizza"
+
+        self._making_pizza = True
+        
+        # MoveTo dough
+        # Click dough
+        doughposx, doughposy = self.get_ingredient_position(f"dough_{self.dough_count}")
+        print(doughposx, doughposy, self.dough_count)
+        self.click(doughposx, doughposy)
+        self.dough_count += 1
+        time.sleep(0.5)
+
+        # For each ingredient
+            # MoveTo ingredient
+            # Click ingredient
+            # Click base
+        base_x, base_y = self.get_ingredient_position("base_0")
+        for ingredient in self.requested_ingredients:
+            time.sleep(0.5)
+            ingredient_x, ingredient_y = self.get_ingredient_position(ingredient)
+            self.click(ingredient_x, ingredient_y)
+
+            self.place_in_circle()
+            # self.click(base_x, base_y)
+        
+        self._making_pizza = False
+
+    ### Actions
+    # CHeck if provided (x, y) coordinates are within the window's bounds
+    def is_in_bounds(self, x, y):
+        if (x + self.window.left > self.window.width + self.window.left) or (y + self.window.top > self.window.height + self.window.top):
+            return False
+        return True
+    
+    # Click on the website
+    def click(self, x, y):
+        print("CLICKED x%s y%s" % (x, y))
+
+        if not self.is_in_bounds:
+            return
+
+        # Calculate (x, y) in terms of the window position
+        x += self.window.left
+        y += self.window.top
+
+        # Move cursor to (x, y)
+        pyautogui.moveTo(x, y, _pause=False)
+
+        # Click mouse
+        pyautogui.click(button="left")
+
+    def drag(self, drag_from, drag_to):
+        if not self.is_in_bounds:
+            return
+
+        from_x, from_y = drag_from
+        to_x, to_y = drag_to
+
+        # Calculate x, y coordinates in terms of the window position
+        from_x += self.window.left
+        from_y += self.window.top
+
+        to_x += self.window.left
+        to_y += self.window.top
+
+        # Move cursor to (from_x, from_y)
+        pyautogui.moveTo(from_x, from_y, _pause=False)
+
+        # Drag cursor to (to_x, to_y)
+        pyautogui.dragTo(to_x, to_y, button="left")
--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import cv2
 import numpy as np
-import pyautogui
-import pygetwindow as gw
 import os
 import time
-import imutils
 import pytesseract
 from pytesseract import Output
 from capture import *
-from game import *
+from game import Game
 
 # Cleanup when script is exited
 import atexit
@@ -17,62 +14,56 @@ def exit_handler():
     os.system("TASKKILL /F /IM scrcpy.exe")
 atexit.register(exit_handler)
 
-
-window_name = "CPH1979"
-w = gw.getWindowsWithTitle(window_name)
-while len(w) == 0:
-    w = gw.getWindowsWithTitle(window_name)
-    time.sleep(0.1)
-w = w[0]
-w.activate()
-
-capture_height, capture_width = w.height, w.width
-print(f"h: {capture_height}, w: {capture_width}")
-
-screen_width = 900
-screen_ratio = None
-
-fixed_x, fixed_y = None, None
-
-mouse_down = False
-drag_start_x, drag_start_y = None, None
-
-def fixedCoordinatesToResolution(x, y):
-    screen_ratio_x, screen_ratio_y = screen_ratio
-    fixed_x, fixed_y = (x / screen_ratio_x), (y / screen_ratio_y)
-    return (fixed_x, fixed_y)
-
-def click(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        mouse_down = True
-        x, y = fixedCoordinatesToResolution(x, y)
-        drag_start_x, drag_start_y = x, y
-    elif event == cv2.EVENT_LBUTTONUP and mouse_down:
-        mouse_down = False
-        x, y = fixedCoordinatesToResolution(x, y)
-        drag(drag_start_x, drag_start_y, x, y)
+# Initialise the game object
+game = Game("CPH1979")
 
 cv2.namedWindow("pizza_game_capture")
-cv2.setMouseCallback("pizza_game_capture", click)
+
+
+dbclick_armed = False
+
+def handle_click(event, x, y, flags, param):
+    global dbclick_armed
+    if event == cv2.EVENT_LBUTTONDOWN:
+        if dbclick_armed:
+            game.make_pizza()
+            dbclick_armed = False
+        else:
+            dbclick_armed = True
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        dbclick_armed = False
+cv2.setMouseCallback("pizza_game_capture", handle_click)
+
 
 resize_scale_percent = (60 / 100)
 
-game = Game(capture_width, capture_height)
 
-while True:
-    # Update game capture size
-    game.update_capture_bounds(w.width, w.height)
-
-    # Capture application screen
-    img = pyautogui.screenshot(region=(w.left, w.top, w.width, w.height))
+def get_frame():
+    img = game.get_screenshot()
     frame = np.array(img)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    return frame
+
+def in_motion(last_frame, frame):
+    motion_detected = False
+
+    return motion_detected
+
+last_frame = get_frame()
+
+while True:
+    start_time = time.time()
+    # Update game capture size
+    game.update_text_bounds()
+
+    # Capture application screen
+    frame = get_frame()
 
     # Convert text image to grayscale and apply binary threshold
     threshold_img = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     # Crop image
     threshold_img = threshold_img[game.text_bounds["y"]:game.text_bounds["height"], game.text_bounds["x"]:game.text_bounds["width"]]
-  
+
     threshold_img = cv2.threshold(threshold_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
     # Extract text data from image
     custom_config = r'--oem 3 --psm 6'
@@ -91,24 +82,25 @@ while True:
             frame = cv2.rectangle(frame, (x, y), (x+width, y+height), (0, 255, 0), 3)
             frame = cv2.putText(frame, details["text"][i], (x, y), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
     
-    print("Text: {%s} | Ingredients: {%s}" % (game.text, game.get_recipe(game.text)))
+    if not dbclick_armed:
+        print("Text: {%s} | Ingredients: {%s}" % (game.text, game.get_ingredients(game.text)))
+    
     # Downscale the frame so it isn't massive
     resized_width = int(frame.shape[1] * resize_scale_percent)
     resized_height = int(frame.shape[0] * resize_scale_percent)
     frame = cv2.resize(frame, dsize=(resized_width, resized_height), interpolation=cv2.INTER_AREA)
+
     # Add an empty border to the frame image so that we can numpy stack it
     frame = cv2.copyMakeBorder(frame, 0, 0, 0, threshold_img.shape[1]-frame.shape[1], borderType=cv2.BORDER_CONSTANT)
-    # Convert the threshold/text image to RGB so that it has the same size as the frame
+    frame = cv2.putText(frame, "{:.2f}".format(time.time() - start_time)+"s", (0, 30), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (0, 0, 255), 3)
+   
+    # Convert the threshold image to RGB so that it has the same size as the frame
     threshold_img = cv2.cvtColor(threshold_img, cv2.COLOR_GRAY2RGB)
-    # Vertically stack the frame and threshold/text images
-    concatenation = np.vstack((frame, threshold_img))
     
+    # Vertically stack the frame and threshold images
+    concatenation = np.vstack((frame, threshold_img))
+
     cv2.imshow("pizza_game_capture", concatenation)
-
-
-    screen_height = frame.shape[0]
-    screen_ratio = (screen_width / capture_width, screen_height / capture_height)
-    # print(screen_ratio, frame.shape, [capture_width, capture_height])
 
     # Quit application when "Q" pressed
     if cv2.waitKey(1) == ord("q"):
